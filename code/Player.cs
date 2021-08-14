1fb415bb0ebd4dbacc1eb813d2e2e5fa0722dd03
Maxime66410 <max66410@hotmail.fr>
2021-08-14 21:39:36 +0200

    Add Human Sound

--- code/Player.cs
+++ code/Player.cs
@@ -44,11 +44,12 @@ public partial class DeathmatchPlayer : Sandbox.Player
 			{
 				HumanSpawnSound();
 			}
-			else
+
+			if ( IsZombie )
 			{
 				ZombieSpawnSound();
 			}
-			
+
 		}
 		else
 		{
@@ -334,14 +335,274 @@ public partial class DeathmatchPlayer : Sandbox.Player
 
 	public void HumanSpawnSound()
 	{
+		Random rnd = new Random();
+
+		var RandomSound = rnd.Next( 0, 63 );
 		
+		if (RandomSound == 0)
+		{
+			PlaySound( "humanspawn01.spawn");
+		}
+		if (RandomSound == 1)
+		{
+			PlaySound( "humanspawn02.spawn");
+		}
+		if (RandomSound == 2)
+		{
+			PlaySound( "humanspawn03.spawn");
+		}
+		if (RandomSound == 3)
+		{
+			PlaySound( "humanspawn04.spawn");
+		}
+		if (RandomSound == 4)
+		{
+			PlaySound( "humanspawn05.spawn");
+		}
+		if (RandomSound == 5)
+		{
+			PlaySound( "humanspawn06.spawn");
+		}
+		if (RandomSound == 6)
+		{
+			PlaySound( "humanspawn07.spawn");
+		}
+		if (RandomSound == 7)
+		{
+			PlaySound( "humanspawn08.spawn");
+		}
+		if (RandomSound == 8)
+		{
+			PlaySound( "humanspawn09.spawn");
+		}
+		if (RandomSound == 9)
+		{
+			PlaySound( "humanspawn10.spawn");
+		}
+		if (RandomSound == 10)
+		{
+			PlaySound( "humanspawn11.spawn");
+		}
+		if (RandomSound == 11)
+		{
+			PlaySound( "humanspawn12.spawn");
+		}
+		if (RandomSound == 12)
+		{
+			PlaySound( "humanspawn13.spawn");
+		}
+		if (RandomSound == 13)
+		{
+			PlaySound( "humanspawn14.spawn");
+		}
+		if (RandomSound == 14)
+		{
+			PlaySound( "humanspawn15.spawn");
+		}
+		if (RandomSound == 15)
+		{
+			PlaySound( "humanspawn16.spawn");
+		}
+		if (RandomSound == 16)
+		{
+			PlaySound( "humanspawn17.spawn");
+		}
+		if (RandomSound == 17)
+		{
+			PlaySound( "humanspawn18.spawn");
+		}
+		if (RandomSound == 18)
+		{
+			PlaySound( "humanspawn19.spawn");
+		}
+		if (RandomSound == 19)
+		{
+			PlaySound( "humanspawn20.spawn");
+		}
+		if (RandomSound == 20)
+		{
+			PlaySound( "humanspawn21.spawn");
+		}
+		if (RandomSound == 21)
+		{
+			PlaySound( "humanspawn22.spawn");
+		}
+		if (RandomSound == 22)
+		{
+			PlaySound( "humanspawn23.spawn");
+		}
+		if (RandomSound == 23)
+		{
+			PlaySound( "humanspawn24.spawn");
+		}
+		if (RandomSound == 24)
+		{
+			PlaySound( "humanspawn25.spawn");
+		}
+		if (RandomSound == 25)
+		{
+			PlaySound( "humanspawn26.spawn");
+		}
+		if (RandomSound == 26)
+		{
+			PlaySound( "humanspawn27.spawn");
+		}
+		if (RandomSound == 27)
+		{
+			PlaySound( "humanspawn28.spawn");
+		}
+		if (RandomSound == 28)
+		{
+			PlaySound( "humanspawn29.spawn");
+		}
+		if (RandomSound == 29)
+		{
+			PlaySound( "humanspawn30.spawn");
+		}
+		if (RandomSound == 30)
+		{
+			PlaySound( "humanspawn31.spawn");
+		}
+		if (RandomSound == 31)
+		{
+			PlaySound( "humanspawn32.spawn");
+		}
+		if (RandomSound == 32)
+		{
+			PlaySound( "humanspawn33.spawn");
+		}
+		if (RandomSound == 33)
+		{
+			PlaySound( "humanspawn34.spawn");
+		}
+		if (RandomSound == 34)
+		{
+			PlaySound( "humanspawn35.spawn");
+		}
+		if (RandomSound == 35)
+		{
+			PlaySound( "humanspawn36.spawn");
+		}
+		if (RandomSound == 36)
+		{
+			PlaySound( "humanspawn37.spawn");
+		}
+		if (RandomSound == 37)
+		{
+			PlaySound( "humanspawn38.spawn");
+		}
+		if (RandomSound == 38)
+		{
+			PlaySound( "humanspawn39.spawn");
+		}
+		if (RandomSound == 39)
+		{
+			PlaySound( "humanspawn40.spawn");
+		}
+		if (RandomSound == 40)
+		{
+			PlaySound( "humanspawn41.spawn");
+		}
+		if (RandomSound == 41)
+		{
+			PlaySound( "humanspawn42.spawn");
+		}
+		if (RandomSound == 42)
+		{
+			PlaySound( "humanspawn43.spawn");
+		}
+		if (RandomSound == 43)
+		{
+			PlaySound( "humanspawn44.spawn");
+		}
+		if (RandomSound == 44)
+		{
+			PlaySound( "humanspawn45.spawn");
+		}
+		if (RandomSound == 45)
+		{
+			PlaySound( "humanspawn46.spawn");
+		}
+		if (RandomSound == 46)
+		{
+			PlaySound( "humanspawn47.spawn");
+		}
+		if (RandomSound == 47)
+		{
+			PlaySound( "humanspawn48.spawn");
+		}
+		if (RandomSound == 48)
+		{
+			PlaySound( "humanspawn49.spawn");
+		}
+		if (RandomSound == 49)
+		{
+			PlaySound( "humanspawn50.spawn");
+		}
+		if (RandomSound == 50)
+		{
+			PlaySound( "humanspawn51.spawn");
+		}
+		if (RandomSound == 51)
+		{
+			PlaySound( "humanspawn52.spawn");
+		}
+		if (RandomSound == 52)
+		{
+			PlaySound( "humanspawn53.spawn");
+		}
+		if (RandomSound == 53)
+		{
+			PlaySound( "humanspawn54.spawn");
+		}
+		if (RandomSound == 54)
+		{
+			PlaySound( "humanspawn55.spawn");
+		}
+		if (RandomSound == 55)
+		{
+			PlaySound( "humanspawn56.spawn");
+		}
+		if (RandomSound == 56)
+		{
+			PlaySound( "humanspawn57.spawn");
+		}
+		if (RandomSound == 57)
+		{
+			PlaySound( "humanspawn58.spawn");
+		}
+		if (RandomSound == 58)
+		{
+			PlaySound( "humanspawn59.spawn");
+		}
+		if (RandomSound == 59)
+		{
+			PlaySound( "humanspawn60.spawn");
+		}
+		if (RandomSound == 60)
+		{
+			PlaySound( "humanspawn61.spawn");
+		}
+		if (RandomSound == 61)
+		{
+			PlaySound( "humanspawn62.spawn");
+		}
+		if (RandomSound == 62)
+		{
+			PlaySound( "humanspawn63.spawn");
+		}
+		if (RandomSound == 63)
+		{
+			PlaySound( "humanspawn64.spawn");
+		}
+
 	}
 
 	public void ZombieSpawnSound()
 	{
-		Random rnd = new Random();
+		Random rnds = new Random();
 
-		var RandomSound = rnd.Next( 0, 11 );
+		var RandomSound = rnds.Next( 0, 11 );
 		
 		if (RandomSound == 0)
 		{
@@ -463,6 +724,169 @@ public partial class DeathmatchPlayer : Sandbox.Player
 
 	public void HumanDieSound()
 	{
+		Random rnd = new Random();
+
+		var RandomSound = rnd.Next( 0, 39 );
 		
+		if (RandomSound == 0)
+		{
+			PlaySound( "humandeath01.die");
+		}
+		if (RandomSound == 1)
+		{
+			PlaySound( "humandeath02.die");
+		}
+		if (RandomSound == 2)
+		{
+			PlaySound( "humandeath03.die");
+		}
+		if (RandomSound == 3)
+		{
+			PlaySound( "humandeath04.die");
+		}
+		if (RandomSound == 4)
+		{
+			PlaySound( "humandeath05.die");
+		}
+		if (RandomSound == 5)
+		{
+			PlaySound( "humandeath06.die");
+		}
+		if (RandomSound == 6)
+		{
+			PlaySound( "humandeath06.die");
+		}
+		if (RandomSound == 7)
+		{
+			PlaySound( "humandeath08.die");
+		}
+		if (RandomSound == 8)
+		{
+			PlaySound( "humandeath09.die");
+		}
+		if (RandomSound == 9)
+		{
+			PlaySound( "humandeath10.die");
+		}
+		if (RandomSound == 10)
+		{
+			PlaySound( "humandeath11.die");
+		}
+		if (RandomSound == 11)
+		{
+			PlaySound( "humandeath12.die");
+		}
+		if (RandomSound == 12)
+		{
+			PlaySound( "humandeath13.die");
+		}
+		if (RandomSound == 13)
+		{
+			PlaySound( "humandeath14.die");
+		}
+		if (RandomSound == 14)
+		{
+			PlaySound( "humandeath15.die");
+		}
+		if (RandomSound == 15)
+		{
+			PlaySound( "humandeath16.die");
+		}
+		if (RandomSound == 16)
+		{
+			PlaySound( "humandeath17.die");
+		}
+		if (RandomSound == 17)
+		{
+			PlaySound( "humandeath18.die");
+		}
+		if (RandomSound == 18)
+		{
+			PlaySound( "humandeath19.die");
+		}
+		if (RandomSound == 19)
+		{
+			PlaySound( "humandeath20.die");
+		}
+		if (RandomSound == 20)
+		{
+			PlaySound( "humandeath21.die");
+		}
+		if (RandomSound == 21)
+		{
+			PlaySound( "humandeath22.die");
+		}
+		if (RandomSound == 22)
+		{
+			PlaySound( "humandeath23.die");
+		}
+		if (RandomSound == 23)
+		{
+			PlaySound( "humandeath24.die");
+		}
+		if (RandomSound == 24)
+		{
+			PlaySound( "humandeath25.die");
+		}
+		if (RandomSound == 25)
+		{
+			PlaySound( "humandeath26.die");
+		}
+		if (RandomSound == 26)
+		{
+			PlaySound( "humandeath27.die");
+		}
+		if (RandomSound == 27)
+		{
+			PlaySound( "humandeath28.die");
+		}
+		if (RandomSound == 28)
+		{
+			PlaySound( "humandeath29.die");
+		}
+		if (RandomSound == 29)
+		{
+			PlaySound( "humandeath30.die");
+		}
+		if (RandomSound == 30)
+		{
+			PlaySound( "humandeath31.die");
+		}
+		if (RandomSound == 31)
+		{
+			PlaySound( "humandeath32.die");
+		}
+		if (RandomSound == 32)
+		{
+			PlaySound( "humandeath33.die");
+		}
+		if (RandomSound == 33)
+		{
+			PlaySound( "humandeath34.die");
+		}
+		if (RandomSound == 34)
+		{
+			PlaySound( "humandeath35.die");
+		}
+		if (RandomSound == 35)
+		{
+			PlaySound( "humandeath36.die");
+		}
+		if (RandomSound == 36)
+		{
+			PlaySound( "humandeath37.die");
+		}
+		if (RandomSound == 37)
+		{
+			PlaySound( "humandeath38.die");
+		}
+		if (RandomSound == 38)
+		{
+			PlaySound( "humandeath39.die");
+		}
+		if (RandomSound == 39)
+		{
+			PlaySound( "humandeath40.die");
+		}
 	}
 }
